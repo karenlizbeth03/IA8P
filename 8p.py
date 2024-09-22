00de2d84255a94fe0eb35cad912da08d0c3fdc92
Karen Moyolema <kmoyolema3943@uta.edu.ec>
2024-09-22 13:57:33 -0500

    puzzle

--- 8p.py
+++ 8p.py
@@ -1,7 +1,7 @@
 import heapq
-import time
-import psutil
 import random
+import time
+import tracemalloc
 
 class Nodo:
     def __init__(self, estado, padre=None, accion=None, costo_g=0, costo_h=0):
@@ -17,29 +17,13 @@ class Nodo:
     def __lt__(self, otro):
         return self.costo_total() < otro.costo_total()
 
-    def obtener_matriz_estado(self):
-        return [list(fila) for fila in self.estado]
-
-    def imprimir_estado(self):
-        for fila in self.estado:
-            print(fila)
-        print()
-
 class Operaciones:
-
-    def calcular_costo_h(self, estado, estado_objetivo):
+    def calcular_costo_h(self, estado):
         # Heurística de fichas mal ubicadas
-        fichas_mal_ubicadas = 0
-        for i in range(3):
-            for j in range(3):
-                if estado[i][j] != 0 and estado[i][j] != estado_objetivo[i][j]:
-                    fichas_mal_ubicadas += 1
+        fichas_mal_ubicadas = sum(1 for i in range(3) for j in range(3) if estado[i][j] != 0 and estado[i][j] != (i * 3 + j))
         return fichas_mal_ubicadas
 
-    def calcular_costo_g(self, nodo):
-        return nodo.costo_g + 1
-
-class Cola():
+class Cola:
     def __init__(self):
         self.cola = []
 
@@ -53,161 +37,84 @@ class Cola():
         heapq.heappush(self.cola, elemento)
 
 class Resolver:
-    def __init__(self, estado_objetivo=None):
-        self.estado_inicial = self.generar_estado_inicial()
-        self.estado_objetivo = estado_objetivo if estado_objetivo else [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    def __init__(self):
+        self.estado_objetivo = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 
     def generar_estado_inicial(self):
-        numeros = list(range(9))
-        random.shuffle(numeros)
-        estado_inicial = [[numeros[i * 3 + j] for j in range(3)] for i in range(3)]
-        return estado_inicial
+        while True:
+            numeros = list(range(9))
+            random.shuffle(numeros)
+            estado_inicial = [[numeros[i * 3 + j] for j in range(3)] for i in range(3)]
+            if self.es_solucionable(estado_inicial):
+                return estado_inicial
 
-    def obtener_acciones_posibles(self, estado):
-        acciones_posibles = []
-        fila_vacio = None
-        columna_vacio = None
+    def es_solucionable(self, estado):
+        estado_1d = [num for fila in estado for num in fila if num != 0]
+        inversiones = sum(1 for i in range(len(estado_1d)) for j in range(i + 1, len(estado_1d)) if estado_1d[i] > estado_1d[j])
+        return inversiones % 2 == 0
+
+    def encontrar_vacio(self, estado):
         for i in range(3):
             for j in range(3):
                 if estado[i][j] == 0:
-                    fila_vacio = i
-                    columna_vacio = j
-                    break
-            if fila_vacio is not None:
-                break
+                    return i, j
 
-        if fila_vacio > 0:
-            acciones_posibles.append('abajo')
-        if fila_vacio < 2:
-            acciones_posibles.append('arriba')
-        if columna_vacio > 0:
-            acciones_posibles.append('derecha')
-        if columna_vacio < 2:
-            acciones_posibles.append('izquierda')
+    def obtener_acciones_posibles(self, estado):
+        acciones_posibles = []
+        fila_vacio, columna_vacio = self.encontrar_vacio(estado)
+
+        if fila_vacio > 0: acciones_posibles.append('arriba')
+        if fila_vacio < 2: acciones_posibles.append('abajo')
+        if columna_vacio > 0: acciones_posibles.append('izquierda')
+        if columna_vacio < 2: acciones_posibles.append('derecha')
 
         return acciones_posibles
 
     def es_estado_objetivo(self, estado):
         return estado == self.estado_objetivo
 
+    def mover(self, estado, direccion):
+        estado_nuevo = [list(fila) for fila in estado]
+        fila_vacio, columna_vacio = self.encontrar_vacio(estado_nuevo)
+
+        if direccion == 'abajo' and fila_vacio < 2:
+            estado_nuevo[fila_vacio][columna_vacio], estado_nuevo[fila_vacio + 1][columna_vacio] = estado_nuevo[fila_vacio + 1][columna_vacio], estado_nuevo[fila_vacio][columna_vacio]
+        elif direccion == 'arriba' and fila_vacio > 0:
+            estado_nuevo[fila_vacio][columna_vacio], estado_nuevo[fila_vacio - 1][columna_vacio] = estado_nuevo[fila_vacio - 1][columna_vacio], estado_nuevo[fila_vacio][columna_vacio]
+        elif direccion == 'derecha' and columna_vacio < 2:
+            estado_nuevo[fila_vacio][columna_vacio], estado_nuevo[fila_vacio][columna_vacio + 1] = estado_nuevo[fila_vacio][columna_vacio + 1], estado_nuevo[fila_vacio][columna_vacio]
+        elif direccion == 'izquierda' and columna_vacio > 0:
+            estado_nuevo[fila_vacio][columna_vacio], estado_nuevo[fila_vacio][columna_vacio - 1] = estado_nuevo[fila_vacio][columna_vacio - 1], estado_nuevo[fila_vacio][columna_vacio]
+
+        return estado_nuevo
+
     def funcion_sucesor(self, movimientos, nodo_padre):
         sucesores = []
-        calculo = Operaciones()
+        operaciones = Operaciones()
         for accion in movimientos:
-            estado_nuevo = nodo_padre.obtener_matriz_estado()
-            fila_vacio = None
-            columna_vacio = None
-
-            for i in range(3):
-                for j in range(3):
-                    if estado_nuevo[i][j] == 0:
-                        fila_vacio = i
-                        columna_vacio = j
-                        break
-                if fila_vacio is not None:
-                    break
-
-            if accion == 'abajo':
-                estado_nuevo[fila_vacio][columna_vacio], estado_nuevo[fila_vacio - 1][columna_vacio] = estado_nuevo[fila_vacio - 1][columna_vacio], estado_nuevo[fila_vacio][columna_vacio]
-            elif accion == 'arriba':
-                estado_nuevo[fila_vacio][columna_vacio], estado_nuevo[fila_vacio + 1][columna_vacio] = estado_nuevo[fila_vacio + 1][columna_vacio], estado_nuevo[fila_vacio][columna_vacio]
-            elif accion == 'derecha':
-                estado_nuevo[fila_vacio][columna_vacio], estado_nuevo[fila_vacio][columna_vacio - 1] = estado_nuevo[fila_vacio][columna_vacio - 1], estado_nuevo[fila_vacio][columna_vacio]
-            elif accion == 'izquierda':
-                estado_nuevo[fila_vacio][columna_vacio], estado_nuevo[fila_vacio][columna_vacio + 1] = estado_nuevo[fila_vacio][columna_vacio + 1], estado_nuevo[fila_vacio][columna_vacio]
-
-            costo_g_nuevo = calculo.calcular_costo_g(nodo_padre)
-            costo_h_nuevo = calculo.calcular_costo_h(estado_nuevo, self.estado_objetivo)
+            estado_nuevo = self.mover(nodo_padre.estado, accion)
+            costo_g_nuevo = nodo_padre.costo_g + 1
+            costo_h_nuevo = operaciones.calcular_costo_h(estado_nuevo)
             nodo_nuevo = Nodo(estado_nuevo, nodo_padre, accion, costo_g_nuevo, costo_h_nuevo)
             sucesores.append(nodo_nuevo)
         return sucesores
 
     def imprimir_pasos(self, nodo):
         pasos = []
-        while nodo.accion is not None:
-            pasos.append(nodo.accion)
+        while nodo:
+            pasos.append(nodo)
             nodo = nodo.padre
         pasos.reverse()
-        estado_actual = self.estado_inicial
-        for i, paso in enumerate(pasos, start=1):
-            print("Paso {}: mover {}".format(i, paso))
-            if paso == 'arriba':
-                estado_actual = self.mover_arriba(estado_actual)
-            elif paso == 'abajo':
-                estado_actual = self.mover_abajo(estado_actual)
-            elif paso == 'izquierda':
-                estado_actual = self.mover_izquierda(estado_actual)
-            elif paso == 'derecha':
-                estado_actual = self.mover_derecha(estado_actual)
-            for fila in estado_actual:
+
+        for i, nodo in enumerate(pasos, start=1):
+            print(f"Paso {i}: mover {nodo.accion}")
+            for fila in nodo.estado:
                 print(fila)
-            print()
-
-    def mover_arriba(self, estado):
-        estado_nuevo = [list(fila) for fila in estado]
-        fila_vacio = None
-        columna_vacio = None
-        for i in range(3):
-            for j in range(3):
-                if estado_nuevo[i][j] == 0:
-                    fila_vacio = i
-                    columna_vacio = j
-                    break
-            if fila_vacio is not None:
-                break
-        estado_nuevo[fila_vacio][columna_vacio], estado_nuevo[fila_vacio + 1][columna_vacio] = estado_nuevo[fila_vacio + 1][columna_vacio], estado_nuevo[fila_vacio][columna_vacio]
-        return estado_nuevo
-
-    def mover_abajo(self, estado):
-        estado_nuevo = [list(fila) for fila in estado]
-        fila_vacio = None
-        columna_vacio = None
-        for i in range(3):
-            for j in range(3):
-                if estado_nuevo[i][j] == 0:
-                    fila_vacio = i
-                    columna_vacio = j
-                    break
-            if fila_vacio is not None:
-                break
-        estado_nuevo[fila_vacio][columna_vacio], estado_nuevo[fila_vacio - 1][columna_vacio] = estado_nuevo[fila_vacio - 1][columna_vacio], estado_nuevo[fila_vacio][columna_vacio]
-        return estado_nuevo
-
-    def mover_izquierda(self, estado):
-        estado_nuevo = [list(fila) for fila in estado]
-        fila_vacio = None
-        columna_vacio = None
-        for i in range(3):
-            for j in range(3):
-                if estado_nuevo[i][j] == 0:
-                    fila_vacio = i
-                    columna_vacio = j
-                    break
-            if fila_vacio is not None:
-                break
-        estado_nuevo[fila_vacio][columna_vacio], estado_nuevo[fila_vacio][columna_vacio + 1] = estado_nuevo[fila_vacio][columna_vacio + 1], estado_nuevo[fila_vacio][columna_vacio]
-        return estado_nuevo
-
-    def mover_derecha(self, estado):
-        estado_nuevo = [list(fila) for fila in estado]
-        fila_vacio = None
-        columna_vacio = None
-        for i in range(3):
-            for j in range(3):
-                if estado_nuevo[i][j] == 0:
-                    fila_vacio = i
-                    columna_vacio = j
-                    break
-            if fila_vacio is not None:
-                break
-        estado_nuevo[fila_vacio][columna_vacio], estado_nuevo[fila_vacio][columna_vacio - 1] = estado_nuevo[fila_vacio][columna_vacio - 1], estado_nuevo[fila_vacio][columna_vacio]
-        return estado_nuevo
+            print(f"Costo g: {nodo.costo_g}, Costo h: {nodo.costo_h}, Costo total f: {nodo.costo_total()}\n")
 
     def Resolver_8_puzzle(self):
-        estado_inicial = self.estado_inicial
-        estado_objetivo = self.estado_objetivo
-
-        nodo_inicial = Nodo(estado_inicial, None, None, 0, Operaciones().calcular_costo_h(estado_inicial, estado_objetivo))
+        estado_inicial = self.generar_estado_inicial()
+        nodo_inicial = Nodo(estado_inicial, None, None, 0, Operaciones().calcular_costo_h(estado_inicial))
 
         frontera = Cola()
         frontera.agregar_elemento(nodo_inicial)
@@ -215,24 +122,36 @@ class Resolver:
         visitados = set()
         visitados.add(tuple(map(tuple, estado_inicial)))
 
+        nodos_visitados = 0
+        all_nodes = []
+
+        start_time = time.time()
+        tracemalloc.start()
+
         while not frontera.cola_vacia():
             nodo_actual = frontera.sacar_elemento()
+            nodos_visitados += 1
+            all_nodes.append(nodo_actual)
 
             if self.es_estado_objetivo(nodo_actual.estado):
                 self.imprimir_pasos(nodo_actual)
-                return nodo_actual
+                break
 
-            movimientos = self.obtener_acciones_posibles(nodo_actual.estado)
-            sucesores = self.funcion_sucesor(movimientos, nodo_actual)
-
-            for sucesor in sucesores:
-                estado_tuple = tuple(map(tuple, sucesor.estado))
-                if estado_tuple not in visitados:
+            movimientos_posibles = self.obtener_acciones_posibles(nodo_actual.estado)
+            for sucesor in self.funcion_sucesor(movimientos_posibles, nodo_actual):
+                if tuple(map(tuple, sucesor.estado)) not in visitados:
+                    visitados.add(tuple(map(tuple, sucesor.estado)))
                     frontera.agregar_elemento(sucesor)
-                    visitados.add(estado_tuple)
 
-        return None
+        end_time = time.time()
+        tiempo_total = end_time - start_time
+        memoria_consumida = tracemalloc.get_traced_memory()[1]
+        print(f"\nMedidas de rendimiento:")
+        print(f"Nodos visitados (válidos): {nodos_visitados}")
+        print(f"Total de nodos generados: {len(all_nodes)}")
+        print(f"Tiempo total de ejecución: {tiempo_total:.4f} segundos")
+        print(f"Memoria RAM total consumida: {memoria_consumida / 1024:.2f} bytes")
 
-if __name__ == '__main__':
-    resolver = Resolver()
-    solucion = resolver.Resolver_8_puzzle()
+# Ejecutar el resolver
+resolver = Resolver()
+resolver.Resolver_8_puzzle()
